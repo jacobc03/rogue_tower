46f9e20e9c3bb86c97a1411f0699a988570131b8
vbelephants <vbelephants@gmail.com>
2017-02-12 05:52:23 -0500

    backend logic for server

---
+++ Node/model/User.js
@@ -0,0 +1,2 @@
+// Will be layout for table to store User's data.
+// Database will also store User's current state to do a pause/resume.
---
+++ Node/routes/htmlroutes.js
@@ -0,0 +1,2 @@
+// html routes will contain the routes the user actually connects to
+// as of now it will most likely just serve up the index.html in assets
