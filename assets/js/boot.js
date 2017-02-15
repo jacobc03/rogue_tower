6e3d8f9899397a2620094b0ff4ecb6430502719c
Udachi Wiz <Udachi@Udachi.local>
2017-02-14 19:22:10 -0500

    static battle phase

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -1,16 +1,16 @@
-var RogueTower = {};
-RogueTower.Boot = function(game) {};
-RogueTower.Boot.prototype = {
-    preload: function() {
-        this.load.image('preloaderBar', './Graphics/loading-bar.png');
-    },
-    create: function() {
-        this.input.maxPointers = 1;
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.scale.pageAlignHorizontally = true;
-        this.scale.pageAlignVertically = true;
-        //this.scale.setScreenSize(true);
-        this.state.start('Preloader');
-    }
-};
+// var RogueTower = {};
+// RogueTower.Boot = function(game) {};
+// RogueTower.Boot.prototype = {
+//     preload: function() {
+//         this.load.image('preloaderBar', './Graphics/loading-bar.png');
+//     },
+//     create: function() {
+//         this.input.maxPointers = 1;
+//         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+//         this.scale.pageAlignHorizontally = true;
+//         this.scale.pageAlignVertically = true;
+//         //this.scale.setScreenSize(true);
+//         this.state.start('Preloader');
+//     }
+// };
 
